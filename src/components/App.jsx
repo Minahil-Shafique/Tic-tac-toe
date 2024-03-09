a5b3d26eed519ce3c69bda68bd919e1ce3f977e9
Minahil <137945232+Minahil-Shafique@users.noreply.github.com>
2024-03-10 00:11:29 +0500

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,13 +4,13 @@ import Square from "./Square";
 
 export default function App() {
   const [xIsNext, setXIsNext] = useState(true);
-  const [s, setSquares] = useState(Array(9).fill(null));
+  const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
-    if (s[i] || calculateWinner(s)) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
-    const nextSquares = s.slice(); //copies the array
+    const nextSquares = squares.slice(); //copies the array
     if (xIsNext) {
       nextSquares[i] = "X";
     } else {
@@ -20,82 +20,88 @@ export default function App() {
     setXIsNext(!xIsNext); // changing useState to false
   }
 
-  const winner = calculateWinner(s);
+  const winner = calculateWinner(squares);
   let status;
   if (winner) {
     status = "Winner: " + winner;
+  } else if (!squares.includes(null)) {
+    status = "It's a draw!";
   } else {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
-    let handleReset = () => {
-        setSquares(Array(9).fill(null));
-    }
- 
+  const handleReset = () => {
+    setSquares(Array(9).fill(null));
+  };
+
   return (
     <>
-      {winner ? 
-      ( <div class="container"  >
-      <h1>Winner is {winner}</h1>
-      <button className="reset" onClick={handleReset}>Play Again</button></div>) : 
-      <>
-      <div className="status">{status}</div>
-         <div className="board">
+      {winner || !squares.includes(null) ? (
+        <div class="container">
+          <h1>{status}</h1>
+          <button className="reset" onClick={handleReset}>
+            Play Again
+          </button>
+        </div>
+      ) : (
+        <>
+          <div className="status">{status}</div>
+          <div className="board">
             <div className="board-row">
-          <Square
-            className="bl bt"
-            value={s[0]}
-            onSquareClick={() => handleClick(0)}
-          />
-          <Square
-            
-            value={s[1]}
-            onSquareClick={() => handleClick(1)}
-          />
-          <Square
-            value={s[2]}
-            onSquareClick={() => handleClick(2)}
-          />
+              {[0, 1, 2].map((i) => (
+                <Square
+                  key={i}
+                  className={getClassForSquare(i)}
+                  value={squares[i]}
+                  onSquareClick={() => handleClick(i)}
+                />
+              ))}
             </div>
             <div className="board-row">
-          <Square
-            value={s[3]}
-            onSquareClick={() => handleClick(3)}
-          />
-          <Square
-            className="b"
-            value={s[4]}
-            onSquareClick={() => handleClick(4)}
-          />
-          <Square
-            className="br"
-            value={s[5]}
-            onSquareClick={() => handleClick(5)}
-          />
+              {[3, 4, 5].map((i) => (
+                <Square
+                  key={i}
+                  className={getClassForSquare(i)}
+                  value={squares[i]}
+                  onSquareClick={() => handleClick(i)}
+                />
+              ))}
             </div>
             <div className="board-row">
-          <Square
-            value={s[6]}
-            onSquareClick={() => handleClick(6)}
-          />
-          <Square
-            className="bb"
-            value={s[7]}
-            onSquareClick={() => handleClick(7)}
-          />
-          <Square
-            className="bb br"
-            value={s[8]}
-            onSquareClick={() => handleClick(8)}
-          />
+              {[6, 7, 8].map((i) => (
+                <Square
+                  key={i}
+                  className={getClassForSquare(i)}
+                  value={squares[i]}
+                  onSquareClick={() => handleClick(i)}
+                />
+              ))}
             </div>
-         </div>
-      </>}
-   </>
+          </div>
+        </>
+      )}
+    </>
   );
 }
 
-function calculateWinner(s) {
+function getClassForSquare(index) {
+  const classes = ["square"];
+  if (index === 0 || index === 3 || index === 6) {
+    classes.push("l");
+  }
+  if (index === 2 || index === 5 || index === 8) {
+    classes.push("r");
+  }
+  if (index === 0 || index === 1 || index === 2) {
+    classes.push("t");
+  }
+  if (index === 6 || index === 7 || index === 8) {
+    classes.push("b");
+  }
+  return classes.join(" ");
+}
+
+function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -109,8 +115,8 @@ function calculateWinner(s) {
 
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
-    if (s[a] && s[a] === s[b] && s[a] === s[c]) {
-      return s[a];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
     }
   }
   return null;
